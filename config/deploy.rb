0a12e9da12d524bfcc446584db1a90cca1dc490c
otakao <kazuhawk@gmail.com>
2020-04-18 13:42:24 +0900

    reviced_deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.13.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'freemarket_sample_68b'
+set :application, 'teamB'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:otakao/teamB.git'
