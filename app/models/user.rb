962762809fac2c665e2e13d2b97a97c712089b77
nobou-g <61077726+nobou-g@users.noreply.github.com>
2020-04-22 20:58:24 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ApplicationRecord
   has_many :sold_items, ->{where("buyer_id is not NULL")},foreign_key: 'saler_id', class_name: 'Item'
 
   validates :nickname, :last_name, :first_name, :last_name_furigana, :first_name_furigana, :birthday, presence: true
-  validates :last_name, :first_name, format { with: /^[ぁ-んァ-ヶー一-龠]+＄/ }
-  validates :last_name_furigana, :first_name_furigana, format { with: /^[あ-ん゛゜ぁ-ぉゃ-ょー]+$/}
-  validates :email, format { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶー一-龠]+\z/ }
+  validates :last_name_furigana, :first_name_furigana, format: { with: /\A[あ-ん゛゜ぁ-ぉゃ-ょー]+\z/}
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
