dea4371f4be4488ed1e870f84f215618e2c43646
bananaboy2 <ice.lance.fbc@gmail.com>
2020-05-17 23:49:59 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2020041810554410) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "ancestry", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
