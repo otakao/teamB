8bec42406d8c228494697f6b81c352cb1a13299d
otakao <kazuhawk@gmail.com>
2020-05-17 14:11:00 +0900

    category-complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,17 @@
 class ItemsController < ApplicationController
 
   def index
-  
+    @items = Item.all
+    @parents = Category.where(ancestry: nil)
   end
 
   def new
   end
+  
 
   def show
     @item = Item.find(params[:id])
   end
+
 end
+
