f85081550432221f16defd149392f86f47122798
otakao <kazuhawk@gmail.com>
2020-05-24 10:30:45 +0900

    reviced-carrierwave-file

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,39 @@
 class ItemsController < ApplicationController
-
+  
   def index
     @items = Item.all
-    @images = ItemImage.all
+    @images = ItemImage.all  
   end
 
   def new
+    @category_parent_array = ["選択してください"]
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+   end
+    @items = Item.new
+    @items.item_images.new
   end
-  
 
-  def show2
+  def create
+    @items = Item.new(item_params)
+    if @items.save!
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :discription, :condition, :postage, :prefecture, :shipping_date, :price, :brand_id, item_images_attributes: [ :image, :_destroy]).merge(saler_id: current_user.id)
+  end
+
+
+
+ 
 end
-
