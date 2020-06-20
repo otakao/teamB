30d1c57c472baff7b12044648aada4f0537c4647
naoya-uchiyama <naoya.uchiyama55@gmail.com>
2020-06-20 11:42:33 +0900

    commit

---
+++ app/assets/javascripts/comments.js
@@ -0,0 +1,34 @@
+$(function (){
+  function buildHTML(comment){
+    var html = `<p>
+                  <div class="comment_user_content">
+                   ${comment.user_name}
+                   ${comment.content}
+                  </div>
+                 
+                </p>`
+    return html;
+  }
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.comment-box').append(html);
+      $('.textarea').val('');
+      $('.comment-button').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
